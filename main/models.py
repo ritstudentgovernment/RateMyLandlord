a1eb2aa77f2abecb821e9678dffd90966889d6d1
zujko <zujko@csh.rit.edu>
2016-04-08 14:41:04 -0400

    closes #3, closes #4, closes #5; Created initial models for complex, property, and landlord

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Complex(models.Models):
+    name = models.CharField(max_length=150)
+    landlord = models.ForeignKey(Landlord)
+
+class Property(models.Model):
+    address = models.CharField(max_length=120)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=2)
+    complx = models.ForeignKey(Complex)
+
+class Landlord(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    properties = models.ManyToManyField(Property)
+
