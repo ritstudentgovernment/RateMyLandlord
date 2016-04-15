6b7c5049c747e721a691b8ad7be23e9e62b1dd5b
aaron13michael <ams8560@rit.edu>
2016-04-15 11:37:13 -0400

    started template for search page. Added forms.py with search form

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,8 @@ app_name='main'
 urlpatterns= [
     url(r'^$', views.index, name='index'),
     #url(r'about', views.about, name='about'),
-    #url(r'search/(\w*)', views.search, name='search'),
+    url(r'search/', views.search, name='search'),
+    url(r'search/(\w*)', views.search, name='search_query'),
     #url(r'property/(\w+)/(\w+)', views.property, name='property'),
     #url(r'landlord/(\w+)/(\w+)', views.landlord, name='landlord'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .forms import *
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main\index.html')
+
+def search(request):
+    form = SearchForm()
+    return render(request, 'main\search.html', {'search_form' : form})
