7cf1068837fe0282b92c8572cf812f7c0beb059c
zujko <zujko@csh.rit.edu>
2016-05-09 15:25:49 -0400

    Implement strong password enforcement and use bcrypt to hash passwords. Closes #43

--- RateMyLandlord/settings.py
+++ RateMyLandlord/settings.py
@@ -130,7 +130,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
